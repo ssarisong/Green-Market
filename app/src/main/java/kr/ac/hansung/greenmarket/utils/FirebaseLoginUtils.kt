ab1a8aac7be17c35a748c85557a8b1bbc171cfce
ptm129 <128340448+ptm129@users.noreply.github.com>
2023-10-31 21:35:21 +0900

    Add files via upload

--- app/src/main/java/kr/ac/hansung/greenmarket/utils/FirebaseLoginUtils.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/utils/FirebaseLoginUtils.kt
@@ -1,6 +1,7 @@
 package kr.ac.hansung.greenmarket.utils
 
 import android.util.Log
+import com.example.greenmarket.LoginActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -13,6 +14,18 @@ class FirebaseLoginUtils{
                     callback(uid)
                 } else {
                     Log.w("LoginActivity", "signInWithEmail", task.exception)
+                    callback(null)
+                }
+            }
+    }
+
+    fun doSignup(userEmail: String, password: String, name: String, birth: String, callback: (String?) -> Unit){
+        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener{task ->
+                if(task.isSuccessful){
+                    callback(null)
+                }else{
+                    Log.w("LoginActivity", "signUpWithEmail", task.exception)
                     callback(null)
                 }
             }
