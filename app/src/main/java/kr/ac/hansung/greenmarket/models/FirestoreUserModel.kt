7e0637d08af393ec32bb5d8a703f4092d8a1531e
ssarisong <thdtnwls2989@naver.com>
2023-11-02 12:40:40 +0900

    :hammer: Add FirestoreUserModel funtions
    deleteUser(), getUserDetail()

--- app/src/main/java/kr/ac/hansung/greenmarket/models/FirestoreUserModel.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/models/FirestoreUserModel.kt
@@ -6,13 +6,49 @@ import android.util.Log
 class FirestoreUserModel {
     val db = FirebaseFirestore.getInstance()
 
-    fun insertUser(uid: String, user: User) {
+    companion object {
+        const val SUCCESS = 1
+        const val FAILURE = 0
+    }
+
+    fun insertUser(uid: String, user: User, callback: (Int) -> Unit) {
         db.collection("User").document(uid).set(user)
             .addOnSuccessListener {
-                Log.d("Firestore", "${user.NAME}(${user.EMAIL}) added with ID: ${uid}")
+                Log.d("FirestoreUserModel", "${user.NAME}(${user.EMAIL}) 사용자 성공적으로 생성 -> ID: [${uid}]")
+                callback(SUCCESS)
             }
             .addOnFailureListener { e ->
-                Log.w("Firestore", "Error adding document", e)
+                Log.w("FirestoreUserModel", "사용자 생성 중 에러 발생!!! -> ", e)
+                callback(FAILURE)
+            }
+    }
+
+    fun deleteUser(uid: String, callback: (Int) -> Unit) {
+        db.collection("User").document(uid).delete()
+            .addOnSuccessListener {
+                Log.d("FirestoreUserModel", "ID: [${uid}] 사용자 성공적으로 삭제")
+                callback(SUCCESS)
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreUserModel", "ID: [${uid}] 사용자 삭제 중 에러 발생!!! -> ", e)
+                callback(FAILURE)
+            }
+    }
+
+    fun getUserDetail(uid: String,  callback: (Int, User?) -> Unit) {
+        db.collection("User").document(uid).get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    val user = documentSnapshot.toObject(User::class.java)
+                    callback(SUCCESS, user)
+                } else {
+                    Log.d("FirestoreUserModel", "ID: [${uid}] 사용자가 존재하지 않음")
+                    callback(FAILURE, null)
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreUserModel", "ID: [${uid}] 사용자를 불러오는 중 에러 발생!!! -> ", e)
+                callback(FAILURE, null)
             }
     }
 }
