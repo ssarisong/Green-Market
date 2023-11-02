06c6e1667a158ebd2f2574d745bc9768ccc6525e
ssarisong <thdtnwls2989@naver.com>
2023-11-02 11:16:07 +0900

    :fire: Fix doSignup
    Move to FirebaseLoginUtils and remove login_signup

--- app/src/main/java/kr/ac/hansung/greenmarket/utils/FirebaseLoginUtils.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/utils/FirebaseLoginUtils.kt
@@ -1,7 +1,6 @@
 package kr.ac.hansung.greenmarket.utils
 
 import android.util.Log
-import com.example.greenmarket.LoginActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -13,28 +12,22 @@ class FirebaseLoginUtils{
                     val uid = Firebase.auth.currentUser?.uid
                     callback(uid)
                 } else {
-                    Log.w("LoginActivity", "signInWithEmail", task.exception)
+                    Log.w("FirebaseLoginUtils", "doLogin", task.exception)
                     callback(null)
                 }
             }
     }
 
-//    fun doSignup (userEmail: String, password: String, name: String, birth: String){
-//        val userEmail = findViewById<EditText>(R.id.editText_signup_id)?.text.toString()
-//        val password = findViewById<EditText>(R.id.editText_signup_pw)?.text.toString()
-//        val name = findViewById<EditText>(R.id.editText_signup_name)?.text.toString()
-//        val birth = findViewById<EditText>(R.id.editText_signup_birth)?.text.toString()
-//
-//        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
-//            .addOnCompleteListener(this){
-//                if(it.isSuccessful){
-//                    startActivity(
-//                        Intent(this, MainActivity::class.java))
-//                    finish()
-//                }else{
-//                    Log.w("LoginActivity", "signUpWithEmail", it.exception)
-//                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//    }
+    fun doSignup(userEmail: String, password: String, name: String, birth: String, callback: (String?) -> Unit){
+        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener{task ->
+                if(task.isSuccessful){
+                    val uid = Firebase.auth.currentUser?.uid
+                    callback(uid)
+                }else{
+                    Log.w("FirebaseLoginUtils", "doSignup", task.exception)
+                    callback(null)
+                }
+            }
+    }
 }
