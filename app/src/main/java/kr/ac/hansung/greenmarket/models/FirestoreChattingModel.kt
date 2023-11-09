ed8f52e26b6dc488397a48a26eb9b8027c065791
ssarisong <thdtnwls2989@naver.com>
2023-11-09 16:43:49 +0900

    :hammer: Add Chat Functions
    add fetchAllMessages, getUserChatRooms

--- app/src/main/java/kr/ac/hansung/greenmarket/models/FirestoreChattingModel.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/models/FirestoreChattingModel.kt
@@ -95,14 +95,66 @@ class FirestoreChattingModel {
      * @param callback 메시지 전송 성공 또는 실패 시 상태 코드(StatusCode)를 인자로 받는 콜백 함수입니다.
      */
     fun sendMessage(chatRoomId: String, message: Chat, callback: (Int) -> Unit) {
-        db.collection("ChatRoom").document(chatRoomId).collection("messages")
+        db.collection("ChatRoom").document(chatRoomId).collection("Chat")
             .add(message)
             .addOnSuccessListener {
-                callback(StatusCode.SUCCESS)
+                val chatRoomRef = db.collection("ChatRoom").document(chatRoomId)
+                chatRoomRef.update("lastMessage", message.message, "lastMessageAt", message.createdAt)
+                    .addOnSuccessListener {
+                        callback(StatusCode.SUCCESS)
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("FirestoreChattingModel", "최신 메시지 업데이트 실패: ", e)
+                        callback(StatusCode.FAILURE)
+                    }
             }
             .addOnFailureListener { e ->
                 Log.w("FirestoreChattingModel", "메시지 보내기 실패: ", e)
                 callback(StatusCode.FAILURE)
             }
     }
+    /**
+     * Firestore에 새 채팅방을 생성합니다.
+     *
+     * @param chatRoom 채팅방 데이터를 가진 ChatRoom 객체입니다.
+     * @param callback 채팅방 생성 성공 또는 실패 시 상태 코드(StatusCode)와 채팅방 ID를 인자로 받는 콜백 함수입니다.
+     */
+    fun createChatRoom(chatRoom: ChatRoom, callback: (Int, String) -> Unit) {
+        val newChatRoomRef = db.collection("ChatRoom").document()
+
+        newChatRoomRef.set(chatRoom)
+            .addOnSuccessListener {
+                Log.d("FirestoreChattingModel", "채팅방 정보 성공적으로 생성 -> ID: [${newChatRoomRef.id}]")
+                callback(StatusCode.SUCCESS, newChatRoomRef.id)
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreChattingModel", "채팅방 정보 생성 중 에러 발생!!! -> ", e)
+                callback(StatusCode.FAILURE, newChatRoomRef.id)
+            }
+    }
+
+    /**
+     * 특정 채팅방의 모든 메시지를 조회하는 함수입니다.
+     *
+     * @param chatRoomId 메시지를 조회할 채팅방의 고유 ID입니다.
+     * @param callback 메시지 목록 조회 성공 또는 실패 시 상태 코드(StatusCode)와 메시지 리스트(List<Chat>)를 인자로 받는 콜백 함수입니다.
+     */
+    suspend fun getAllMessages(chatRoomId: String, callback: (Int, List<Chat>?) -> Unit) = withContext(Dispatchers.IO) {
+        try {
+            val messages = db.collection("ChatRoom")
+                .document(chatRoomId)
+                .collection("Chat")
+                .orderBy("createdAt", Query.Direction.ASCENDING)
+                .get()
+                .await()
+                .documents
+                .mapNotNull { it.toObject(Chat::class.java) }
+
+            callback(StatusCode.SUCCESS, messages)
+        } catch (e: Exception) {
+            Log.w("FirestoreChattingModel", "메시지 목록 가져오기 실패, 채팅방 ID: $chatRoomId, 오류: $e")
+            callback(StatusCode.FAILURE, null)
+        }
+    }
+
 }
