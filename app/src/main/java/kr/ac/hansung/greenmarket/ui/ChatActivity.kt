3f20694eb3256a2cd1b433e2dd4eda309f25081b
cwlee <chaewon1662@naver.com>
2023-11-09 11:53:59 +0900

    11/9(1)

---
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatActivity.kt
@@ -0,0 +1,22 @@
+package kr.ac.hansung.greenmarket.ui
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import kr.ac.hansung.greenmarket.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat) // activity_chat.xml을 연결
+
+        /*
+        val messageInput = findViewById<EditText>(R.id.messageInput)
+        val chatListView = findViewById<ListView>(R.id.chatListView)
+        val sendButton = findViewById<Button>(R.id.sendButton)
+        */
+
+    }
+}
