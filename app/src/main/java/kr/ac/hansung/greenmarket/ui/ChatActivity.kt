95ea944784bacc5a341ccb327f3db4fb0fc7286d
cwlee <chaewon1662@naver.com>
2023-11-10 11:06:07 +0900

    11/10(1)

--- app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatActivity.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatActivity.kt
@@ -1,22 +1,64 @@
 package kr.ac.hansung.greenmarket.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.hansung.greenmarket.R
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
-import kr.ac.hansung.greenmarket.R
+import android.widget.ImageButton
+import com.google.firebase.Timestamp
+import kr.ac.hansung.greenmarket.models.Chat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var recyclerView: RecyclerView // Declare recyclerView as a member variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat) // activity_chat.xml을 연결
 
-        /*
-        val messageInput = findViewById<EditText>(R.id.messageInput)
-        val chatListView = findViewById<ListView>(R.id.chatListView)
-        val sendButton = findViewById<Button>(R.id.sendButton)
-        */
 
+        val btnQuit = findViewById<ImageButton>(R.id.btn_quit)
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+        val edtMessage = findViewById<EditText>(R.id.edt_message)
+
+        recyclerView = findViewById(R.id.recycler_messages) // Initialize recyclerView here
+
+        btnQuit.setOnClickListener { // 이전 버튼
+            finish()
+        }
+
+        // RecyclerView 설정
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        chatAdapter = ChatAdapter(mutableListOf()) // 초기에 빈 리스트로 설정
+        recyclerView.adapter = chatAdapter
+
+        btnSubmit.setOnClickListener {
+            // 전송 버튼 클릭 시 호출되는 함수
+            val message = edtMessage.text.toString().trim()
+            if (message.isNotEmpty()) {
+                val currentTime = getCurrentTime()
+                val chatItem = Chat("user1", message, currentTime)
+                addMessage(chatItem)
+                edtMessage.text.clear() // 메시지 전송 후 입력창 초기화
+            }
+        }
+    }
+
+    private fun getCurrentTime(): Timestamp {
+        // 현재 시간을 가져와서 Timestamp로 변환
+        val currentTime = Calendar.getInstance().time
+        return Timestamp(currentTime)
+    }
+
+    private fun addMessage(chatItem: Chat) {
+        // RecyclerView에 메시지 추가
+        chatAdapter.addChat(chatItem)
+        // 스크롤을 가장 최근 메시지로 이동
+        recyclerView.scrollToPosition(chatAdapter.itemCount - 1)
     }
 }
--- app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatlistActivity.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatlistActivity.kt
@@ -3,30 +3,43 @@ package kr.ac.hansung.greenmarket.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
 import kr.ac.hansung.greenmarket.R
+import kr.ac.hansung.greenmarket.models.Chat
+import kr.ac.hansung.greenmarket.models.ChatRoom
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatlistActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatlist)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_menu)
-        bottomNav.selectedItemId = R.id.chatting
+        // RecyclerView 설정
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_chatlist)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    true
-                }
-                R.id.mypage -> {
-                    startActivity(Intent(this, MypageActivity::class.java))
-                    true
-                }
-                else -> false
-            }
+        val chatRoomList = listOf(
+            ChatRoom("product1", "user1", "seller1", Timestamp.now(), null, null),
+            ChatRoom("product2", "user2", "seller2", Timestamp.now(), null, null),
+            // Add more chat room items as needed
+        )
+
+        // 어댑터 설정
+        val chatListAdapter = ChatListAdapter(chatRoomList) { clickedChatRoom ->
+            // 클릭된 채팅 아이템의 정보를 받아옴
+            // 여기에서 ChatActivity로 전환하는 Intent를 생성하고 시작
+            val intent = Intent(this, ChatActivity::class.java)
+            // 필요한 경우 intent에 데이터를 추가
+            intent.putExtra("chatRoomId", clickedChatRoom.productId)
+            // ...
+
+            startActivity(intent)
         }
-
+        recyclerView.adapter = chatListAdapter
     }
 }
+
