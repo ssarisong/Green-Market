29ab12b5995a6b3ed93e50b92a11fc66b0f10dd3
cwlee <chaewon1662@naver.com>
2023-11-10 11:34:34 +0900

    11/10(2)

--- app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatActivity.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatActivity.kt
@@ -1,64 +1,87 @@
 package kr.ac.hansung.greenmarket.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kr.ac.hansung.greenmarket.R
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
-import com.google.firebase.Timestamp
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.hansung.greenmarket.R
+import kr.ac.hansung.greenmarket.StatusCode
 import kr.ac.hansung.greenmarket.models.Chat
-import java.text.SimpleDateFormat
+import kr.ac.hansung.greenmarket.utils.FirebaseChattingUtil
 import java.util.*
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var firebaseChattingUtil: FirebaseChattingUtil
+    private lateinit var chatRoomId: String
     private lateinit var chatAdapter: ChatAdapter
-    private lateinit var recyclerView: RecyclerView // Declare recyclerView as a member variable
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat) // activity_chat.xml을 연결
+        setContentView(R.layout.activity_chat)
+
+        firebaseChattingUtil = FirebaseChattingUtil()
+
+        chatRoomId = intent.getStringExtra("chatRoomId") ?: ""
 
 
         val btnQuit = findViewById<ImageButton>(R.id.btn_quit)
         val btnSubmit = findViewById<Button>(R.id.btn_submit)
         val edtMessage = findViewById<EditText>(R.id.edt_message)
 
-        recyclerView = findViewById(R.id.recycler_messages) // Initialize recyclerView here
-
-        btnQuit.setOnClickListener { // 이전 버튼
-            finish()
-        }
-
-        // RecyclerView 설정
+        recyclerView = findViewById(R.id.recycler_messages)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        chatAdapter = ChatAdapter(mutableListOf()) // 초기에 빈 리스트로 설정
+        chatAdapter = ChatAdapter(mutableListOf())
         recyclerView.adapter = chatAdapter
 
-        btnSubmit.setOnClickListener {
-            // 전송 버튼 클릭 시 호출되는 함수
-            val message = edtMessage.text.toString().trim()
-            if (message.isNotEmpty()) {
-                val currentTime = getCurrentTime()
-                val chatItem = Chat("user1", message, currentTime)
-                addMessage(chatItem)
-                edtMessage.text.clear() // 메시지 전송 후 입력창 초기화
+        chatRoomId = intent.getStringExtra("chatRoomId") ?: ""
+
+
+        val messageListener = firebaseChattingUtil.listenForMessages(chatRoomId) { status, messages ->
+            if (status == StatusCode.SUCCESS) {
+                messages?.let {
+                    chatAdapter.addChatList(it)
+                    recyclerView.scrollToPosition(chatAdapter.itemCount - 1)
+                }
+            } else {
+                Toast.makeText(this, "Failed to listen for messages", Toast.LENGTH_SHORT).show()
             }
         }
-    }
 
-    private fun getCurrentTime(): Timestamp {
-        // 현재 시간을 가져와서 Timestamp로 변환
-        val currentTime = Calendar.getInstance().time
-        return Timestamp(currentTime)
-    }
+        // 생명주기 이벤트 수신을 위해 LifecycleObserver를 구현한 익명 클래스를 생성
+        val lifecycleObserver = object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                messageListener.invoke()
+            }
+        }
 
-    private fun addMessage(chatItem: Chat) {
-        // RecyclerView에 메시지 추가
-        chatAdapter.addChat(chatItem)
-        // 스크롤을 가장 최근 메시지로 이동
-        recyclerView.scrollToPosition(chatAdapter.itemCount - 1)
+        // 생명주기 이벤트 옵저버 등록
+        lifecycle.addObserver(lifecycleObserver)
+
+        btnSubmit.setOnClickListener {
+            val message = edtMessage.text.toString().trim()
+            if (message.isNotEmpty()) {
+                firebaseChattingUtil.sendMessage("user1", chatRoomId, message) { statusCode ->
+                    if (statusCode == StatusCode.SUCCESS) {
+                        edtMessage.text.clear()
+                    } else {
+                        Toast.makeText(this, "Failed to send message", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        btnQuit.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatlistActivity.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ChatlistActivity.kt
@@ -2,21 +2,28 @@ package kr.ac.hansung.greenmarket.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Timestamp
 import kr.ac.hansung.greenmarket.R
+import kr.ac.hansung.greenmarket.StatusCode
 import kr.ac.hansung.greenmarket.models.Chat
 import kr.ac.hansung.greenmarket.models.ChatRoom
+import kr.ac.hansung.greenmarket.utils.FirebaseChattingUtil
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ChatlistActivity : AppCompatActivity() {
 
+    private lateinit var firebaseChattingUtil: FirebaseChattingUtil
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatlist)
+
+        firebaseChattingUtil = FirebaseChattingUtil()
 
         // RecyclerView 설정
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_chatlist)
@@ -32,14 +39,21 @@ class ChatlistActivity : AppCompatActivity() {
         val chatListAdapter = ChatListAdapter(chatRoomList) { clickedChatRoom ->
             // 클릭된 채팅 아이템의 정보를 받아옴
             // 여기에서 ChatActivity로 전환하는 Intent를 생성하고 시작
-            val intent = Intent(this, ChatActivity::class.java)
-            // 필요한 경우 intent에 데이터를 추가
-            intent.putExtra("chatRoomId", clickedChatRoom.productId)
-            // ...
-
-            startActivity(intent)
+            firebaseChattingUtil.createChatRoom(
+                clickedChatRoom.productId,
+                clickedChatRoom.buyerId,
+                clickedChatRoom.sellerId
+            ) { status, chatRoomId ->
+                if (status == StatusCode.SUCCESS) {
+                    val intent = Intent(this, ChatActivity::class.java)
+                    intent.putExtra("chatRoomId", chatRoomId)
+                    startActivity(intent)
+                } else {
+                    // Handle failure case
+                    Toast.makeText(this, "Failed to create chat room", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
         recyclerView.adapter = chatListAdapter
     }
 }
-
