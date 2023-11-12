08ab6770f404bd05f3bb8dc2832ee4ef5ccaab1c
ptm129 <128340448+ptm129@users.noreply.github.com>
2023-11-13 00:28:13 +0900

    Add Product, Category Data Classes

---
+++ app/src/main/java/kr/ac/hansung/greenmarket/models/Category.kt
@@ -0,0 +1,6 @@
+package kr.ac.hansung.greenmarket.models
+
+data class Category (
+    val catId: String,
+    val catNm: String,
+)
---
+++ app/src/main/java/kr/ac/hansung/greenmarket/models/Product.kt
@@ -0,0 +1,8 @@
+package kr.ac.hansung.greenmarket.models
+
+data class Product(
+    val productDetail: String,
+    val productImg: String,
+    val productNm: String,
+    val productPrice: String,
+    )
