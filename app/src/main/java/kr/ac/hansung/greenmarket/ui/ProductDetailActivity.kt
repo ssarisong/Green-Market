b7f7c25903d7630aeffae4729ec33f6eff673cd6
cwlee <chaewon1662@naver.com>
2023-11-25 20:47:29 +0900

    11/25

--- app/src/main/java/kr/ac/hansung/greenmarket/ui/ProductDetailActivity.kt
+++ app/src/main/java/kr/ac/hansung/greenmarket/ui/ProductDetailActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kr.ac.hansung.greenmarket.R
 import kr.ac.hansung.greenmarket.StatusCode
 import kr.ac.hansung.greenmarket.utils.FirebaseChattingUtil
@@ -50,16 +51,34 @@ class ProductDetailActivity : AppCompatActivity() {
         quitButton.setOnClickListener {// 이전 버튼
             startActivity(Intent(this, HomeActivity::class.java))
         }
-        chatButton.setOnClickListener {// 채팅하기 버튼
+        chatButton.setOnClickListener {  // 채팅하기 버튼
             productUtil.getProductById(productId) { STATUS_CODE, product ->
-                if(STATUS_CODE == StatusCode.SUCCESS){
-                    chatUtil.createChatRoom(productId = productId?:"", buyerId = userUtil.whoAmI()?.uid?:"", sellerId = product?.sellerId ?:"") { STATUS_CODE, newChatRoomId ->
-                        if(STATUS_CODE == StatusCode.SUCCESS){
-                            val intent = Intent(this, ChatActivity::class.java)
-                            intent.putExtra("chatRoomId", newChatRoomId)
-                            startActivity(intent)
+                if (STATUS_CODE == StatusCode.SUCCESS) {
+                    chatUtil.getUserChatRooms(userUtil.whoAmI()?.uid ?: "") { chatRoomsStatus, chatRooms ->
+                        if (chatRoomsStatus == StatusCode.SUCCESS) {
+                            val existingChatRoom = chatRooms?.find { chatRoom ->
+                                chatRoom.productId == productId && (chatRoom.buyerId == userUtil.whoAmI()?.uid ?: "" || chatRoom.sellerId == product?.sellerId ?: "")
+                            }
+
+                            if (existingChatRoom != null) {
+                                // 존재하는 채팅방 불러오기
+                                val intent = Intent(this, ChatActivity::class.java)
+                                intent.putExtra("chatRoomId", existingChatRoom.chatRoomId)
+                                startActivity(intent)
+                            } else {
+                                // 채팅방 생성
+                                chatUtil.createChatRoom(productId = productId ?: "", buyerId = userUtil.whoAmI()?.uid ?: "", sellerId = product?.sellerId ?: "") { newChatRoomStatus, newChatRoomId ->
+                                    if (newChatRoomStatus == StatusCode.SUCCESS) {
+                                        val intent = Intent(this, ChatActivity::class.java)
+                                        intent.putExtra("chatRoomId", newChatRoomId)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(this, "채팅방 생성 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                            }
                         } else {
-                            Toast.makeText(this, "채팅방 생성 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "채팅방 목록을 가져오는 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
@@ -69,6 +88,25 @@ class ProductDetailActivity : AppCompatActivity() {
         }
 
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_menu)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.chatting -> {
+                    startActivity(Intent(this, ChatlistActivity::class.java))
+                    true
+                }
+                R.id.mypage -> {
+                    startActivity(Intent(this, MypageActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun getProductStatusString(stateCode: Int): String {
@@ -79,4 +117,6 @@ class ProductDetailActivity : AppCompatActivity() {
             else -> "알 수 없는 상태"
         }
     }
+
+
 }
